b5980a3dd7a42a6ce07de4520b479ce438e7bbf2
Tyrell-Lewis <tyrell.lewis@my.uwi.edu>
2024-04-18 23:12:45 +0000

    update

--- App/controllers/exercise.py
+++ App/controllers/exercise.py
@@ -12,8 +12,11 @@ def get_all_exercises():
 def remove_html_tags(text):
     # Code sourced from online to clean the text in api, which contained html tags.
     html_tags_pattern = re.compile(r'<[^>]+>')
+
+    nbsp_pattern = re.compile(r'&nbsp;')
     
     clean_text = re.sub(html_tags_pattern, '', text)
+    clean_text = re.sub(nbsp_pattern, '', clean_text)
     return clean_text
 
 
@@ -25,7 +28,7 @@ def create_exercise(name, description):
     return new_exercise
 
 def get_all_exercises_api(limit, start_point):
-    url = f'https://wger.de/api/v2/exerciseinfo/?limit={limit}&offset={start_point}&language=2'
+    url = f'https://wger.de/api/v2/exerciseinfo/?limit={limit}&offset={start_point}'
     #the limit=5 means it only renders the 5 at a time
     #the offset=0 means it starts from the 1st one, which is at 0 like an array list
     # can manipulate the url by using variable for the numbers {{}} or ${} whichever one works for this.
@@ -45,12 +48,12 @@ def get_all_exercises_api(limit, start_point):
 
 
         for exercise_d in exercise_data:
-            #if exercise_d.get('language', 'short_name') == 'en':
+            if exercise_d.get('language', {}).get('short_name') == 'en' or exercise_d.get('language', {}).get('id') == 2 or exercise_d.get('language', {}).get('full_name') == 'English':
                 name = exercise_d.get('name', '')
                 description = exercise_d.get('description', '')
 
                 existing_exercise = exercise.query.filter_by(name=name).first()
-        
+
                 if existing_exercise:
                     pass
                 else:
