d3bf21d42ebc18b8d4c2060c4bcc48fb821518e2
amir persad <amir.persad@my.uwi.edu>
2024-04-20 08:16:04 +0000

    updated ui (button position will be fixed)

--- App/views/workout.py
+++ App/views/workout.py
@@ -8,9 +8,9 @@ from App.controllers import (get_all_workouts, create_workout, add_exer_to_worko
 workout_views = Blueprint('workout_views', __name__, template_folder='../templates')
 
 @workout_views.route('/workouts', methods =['GET'])
-@workout_views.route('/workouts/<int:ex_id', methods =['GET'])
+@workout_views.route('/workouts/<int:ex_id>', methods =['GET'])
 @jwt_required()
-def get_workout_page(ex_id):
+def get_workout_page():
     workouts = get_all_workouts()
     test = get_test()
     return render_template('workouts.html', workouts = workouts, test=test)
@@ -21,5 +21,5 @@ def create_workout_action():
     create_workout(request.form.get('exer_id'), "The workout is working", 81)#this is just a temp to test that workout model is working
     add_exer_to_workout(1, 74, 4, 15)#use forms to get data and make it dynamic
     add_exer_to_workout(1, 81, 500, 75)
-    return redirect (url_for('workout_views.get_workout_page'))
+    return redirect (url_for('workout_views.get_workout_page()'))
     #return redirect (request.referrer)
